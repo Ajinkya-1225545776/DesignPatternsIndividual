b75668079d7acdf49d86126464ca630d9b2c5fea
Ajinkya Pise <112654786+Ajinkya-1225545776@users.noreply.github.com>
2022-10-19 23:27:55 -0700

    Add files via upload

---
+++ factorydesign/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    Person Seller = new Seller("Chicken","eggs");
+    Person Buyer = new Buyer("Mutton","Milk");
+    System.out.println(Seller.getline());
+    System.out.println(Buyer.getline());
+    }
+}
